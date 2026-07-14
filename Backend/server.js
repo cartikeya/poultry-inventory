f80ac2c744085cbb436ef4361f68102ebbff5c90
cartikeya <cartikeya.official@gmail.com>
2026-07-14 10:22:52 +0530

    added mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
-
 app.use(express.json());
+require("dotenv").config();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MONGODB connected"))
+  .catch((err) => console.log(err));
 app.get("/", (req, res) => {
-  // res.send("inventory APIs")
-  res.send("Hello API");
+  res.send("inventory APIs");
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
